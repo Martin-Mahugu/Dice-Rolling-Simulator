2760de76f03a2b1750cb4ea5bbef6bfcbb3aa67e
Martin-Mahugu <kanyimahugu@gmail.com>
2024-07-26 23:11:54 +0300

    dice rolling simulator

---
+++ roll.py
@@ -0,0 +1,62 @@
+import random
+
+def roll_dice():
+
+    dice_art = {
+    1: (
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+    ),
+    2: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    3: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    4: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    5: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    6: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+}
+
+    roll = input("Roll the dice? (yes/no): ")
+
+    while roll.lower() == "Yes".lower():
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+
+        print("dice rolled: {} and {}".format(dice1, dice2))
+        print("\n".join(dice_art[dice1]))
+        print("\n".join(dice_art[dice2]))
+
+        roll = input("Roll again? (Yes/No): ")
+
+roll_dice()
